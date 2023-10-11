02195db2addc5264175fb9251be09fb912ec3203
lyrick <66986241+lyrick17@users.noreply.github.com>
2023-10-11 13:03:31 +0800

    Update mysqli_logs.php
    pinaikli ko yung code, mwehehehhehehehe

--- mysql/mysqli_logs.php
+++ mysql/mysqli_logs.php
@@ -3,118 +3,51 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-
 require("mysqli_connect");
 
-function log_register($email) {
-
-    $query_get = "SELECT user_id FROM users WHERE email = '" . $email . "'";
-
-    $result = mysqli_query($dbcon, $query_get);
-    if ($result) {
-        $row = mysqli_fetch_assoc($result);
-        $activity = "registered";
-        
-        $query_insert = mysqli_prepare($dbcon, "INSERT INTO user_activity_log (user_id, activity_description, activity_date) VALUES (?, ?, NOW())");
-        mysqli_stmt_bind_param($query_insert, 'is', $row['user_id'], $activity);
-        mysqli_stmt_execute($query_insert);
-
-
-        // $_SERVER['REMOTE_ADDR'] - for IP address
-    }
-
-    // Free the result set
-    mysqli_free_result($result);
-}
-
-function log_login($user) {
-
-    $query_get = "SELECT user_id FROM users WHERE username = '" . $user . "'";
-
-    $result = mysqli_query($dbcon, $query_get);
-    if ($result) {
-        $row = mysqli_fetch_assoc($result);
-        $activity = "logged in";
-        
-        $query_insert = mysqli_prepare($dbcon, "INSERT INTO user_activity_log (user_id, activity_description, activity_date) VALUES (?, ?, NOW())");
-        mysqli_stmt_bind_param($query_insert, 'is', $row['user_id'], $activity);
-        mysqli_stmt_execute($query_insert);
-    }
-
-    // Free the result set
-    mysqli_free_result($result);
-}
-
-function log_logout($user) {
-    
-    $query_get = "SELECT user_id FROM users WHERE username = '" . $user . "'";
-
-    $result = mysqli_query($dbcon, $query_get);
-    if ($result) {
-        $row = mysqli_fetch_assoc($result);
-        $activity = "logged out";
-        
-        $query_insert = mysqli_prepare($dbcon, "INSERT INTO user_activity_log (user_id, activity_description, activity_date) VALUES (?, ?, NOW())");
-        mysqli_stmt_bind_param($query_insert, 'is', $row['user_id'], $activity);
-        mysqli_stmt_execute($query_insert);
-    }
-
-    // Free the result set
-    mysqli_free_result($result);
-}
-
-function log_text_translate($user) {
-    
-    $query_get = "SELECT user_id FROM users WHERE username = '" . $user . "'";
-
-    $result = mysqli_query($dbcon, $query_get);
-    if ($result) {
-        $row = mysqli_fetch_assoc($result);
-        $activity = "translation: text-to-text";
-        
-        $query_insert = mysqli_prepare($dbcon, "INSERT INTO user_activity_log (user_id, activity_description, activity_date) VALUES (?, ?, NOW())");
-        mysqli_stmt_bind_param($query_insert, 'is', $row['user_id'], $activity);
-        mysqli_stmt_execute($query_insert);
-    }
-
-    // Free the result set
-    mysqli_free_result($result);
-}
-
-
-// register - needs username
-// login - needs username or email
-// logout - needs username
-// every other action - needs the SESSION username variable 
-    // t-t translate, audio upload, audio download, output download, audio transcript, audio translate
-/*
 function logs($log_act, $user) {
+    // every action of user records a log in db
+    // 1 determine the activity of the user before
+    // 2 finding the user in db and
+    // 3 creating a log
+    $activity = "";
+
+    // 1
+    switch ($log_act) {
+        case "register":
+            $activity = "user registered";
+            break;
+        case "login":
+            $activity = "user logged in";
+            break;
+        case "logout":
+            $activity = "user logged out";
+            break;
+        case "text-to-text":
+            $activity = "translation: text-to-text";
+            break;
+        default:
+            $activity = "unknown activity";
+            break;
+        // more logs to be added
+    }
+
+    // 2
+    $query_get = "SELECT user_id FROM users WHERE username = '" . $user . "'";
     $result = mysqli_query($dbcon, $query_get);
+    
     if ($result) {
         $row = mysqli_fetch_assoc($result);
-    
-        switch ($log_act) {
-            case "register":
-                break;
-            case "login":
-                break;
-            case "logout":
-                break;
-            case "text-to-text":
-                $activity = "translation: text-to-text";
-                break;
-            default:
-                break;
-        }
-        
+
+        // 3
         $query_insert = mysqli_prepare($dbcon, "INSERT INTO user_activity_log (user_id, activity_description, activity_date) VALUES (?, ?, NOW())");
         mysqli_stmt_bind_param($query_insert, 'is', $row['user_id'], $activity);
         mysqli_stmt_execute($query_insert);
+        
     }
-
     // Free the result set
     mysqli_free_result($result);
-}*/
+}
 
 mysqli_close($dbcon);
 ?>
