ce4551522650a49adbc6ab0ee08a190195455665
Yiroo <146502284+Yiroo45-afk@users.noreply.github.com>
2023-10-26 18:10:18 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
+
+sideLinks.forEach(item => {
+    const li = item.parentElement;
+    item.addEventListener('click', () => {
+        sideLinks.forEach(i => {
+            i.parentElement.classList.remove('active');
+        })
+        li.classList.add('active');
+    })
+});
+
+const menuBar = document.querySelector('.content nav .bx.bx-menu');
+const sideBar = document.querySelector('.sidebar');
+
+menuBar.addEventListener('click', () => {
+    sideBar.classList.toggle('close');
+});
+
+const searchBtn = document.querySelector('.content nav form .form-input button');
+const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
+const searchForm = document.querySelector('.content nav form');
+
+searchBtn.addEventListener('click', function (e) {
+    if (window.innerWidth < 576) {
+        e.preventDefault;
+        searchForm.classList.toggle('show');
+        if (searchForm.classList.contains('show')) {
+            searchBtnIcon.classList.replace('bx-search', 'bx-x');
+        } else {
+            searchBtnIcon.classList.replace('bx-x', 'bx-search');
+        }
+    }
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 768) {
+        sideBar.classList.add('close');
+    } else {
+        sideBar.classList.remove('close');
+    }
+    if (window.innerWidth > 576) {
+        searchBtnIcon.classList.replace('bx-x', 'bx-search');
+        searchForm.classList.remove('show');
+    }
+});
+
+const toggler = document.getElementById('theme-toggle');
+
+toggler.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+});
+
+    // Get references to the select elements and the translate button
+    const sourceLanguageSelect = document.getElementById('sourceLanguage');
+    const targetLanguageSelect = document.getElementById('targetLanguage');
+    const yourButtonID = document.getElementById('yourButtonID');
+
+    // Add an event listener to both select elements
+    sourceLanguageSelect.addEventListener('change', toggleTranslateButton);
+    targetLanguageSelect.addEventListener('change', toggleTranslateButton);
+
+    // Function to enable/disable the translate button based on selections
+    function toggleTranslateButton() {
+        if (sourceLanguageSelect.value && targetLanguageSelect.value) {
+            yourButtonID.removeAttribute('disabled');
+        } else {
+            yourButtonID.setAttribute('disabled', 'true');
+        }
+    }
