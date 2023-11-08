ed9d10e69ad717c3880818240932ec84fdbffd6b
lyrick <66986241+lyrick17@users.noreply.github.com>
2023-11-08 15:54:00 +0800

    modified uploadandTranscribe, added newFilename
    added lots of comments para mas maintindihan ang process and modified upload and transcribe button para maging unique yung filename for the user only
    ex.
    filename.mp3 --> filename_1.mp3 
    
    given that userid is 1

--- translation.php
+++ translation.php
@@ -1,5 +1,7 @@
 <?php
 
+// translation.php is for audio to text translation
+
 // Use to inspect contents of json / array objects.
 function dd($value){
 	var_dump($value);
@@ -69,40 +71,70 @@ function validateFormat() {
 
 function getVocals($file) {
 	# Activate the virtual environment
-    # call the separate.py which includes the spleeter code for extracting vocals,
+    # use spleeter for extracting vocals,
     #   and pass the file as argument 
     # then, deactivate virtual environment
     $output = shell_exec("spleeter_env\\Scripts\\activate && python scripts/separate.py " . $file . " && deactivate");
+	#  shell_exec("spleeter_env\\Scripts\\activate")
+	#  $output = shell_exec("spleeter separate -o audio_output " . $file);
+	#  shell_exec("deactivate");
 }
 
-function uploadAndTranscribe($path){
+function uploadAndTranscribe($path, $userid){
 
-	$pathto="audio_files/".$path;
+	// 3. get the name of file and extension separately
+	$filename = pathinfo($path, PATHINFO_FILENAME);
+  	$extension = pathinfo($path, PATHINFO_EXTENSION);
 
-	// get the name of file only, for translating the vocals
-    $filename = pathinfo($_FILES['user_file']['name'], PATHINFO_FILENAME);
+	// Append the user's ID to the filename
+	$newFilename = $filename . "_" . $userid . "." . $extension;
+	// audio files folder
+	$pathto="audio_files/".$newFilename;
 
+	// 4.
+		// make sure audio_files folder is created
+		if(!is_dir("audio_files")){
+			mkdir("audio_files", 0777, true);
+		}
 
-	// modified die() if user did not upload file
-	move_uploaded_file( $_FILES['user_file']['tmp_name'],$pathto) or die(audioError2());
-
-	// separate bg music from vocals using spleeter
-	getVocals($_FILES["user_file"]['full_path']);
+		// check if a file with same name already exists
+		if(file_exists($pathto)){
+			// Handle the error, e.g. by logging it or displaying a message
+			//error_log("A file with the name " . $newFilename . " already exists.");
+			return;
+		}
+	
+	// 5.
+ 	move_uploaded_file( $_FILES['user_file']['tmp_name'],$pathto) or die(audioError2());
+	//move_uploaded_file( $_FILES['user_file']['tmp_name'],$pathto) or die(audioError2());
+	
+	// 6.
+	getVocals($pathto);
 	
 	// make sure to go to php.ini in xampp (config > php.ini) 
 	// and set max_execution_time into 600 [10 minutes] or higher (write in seconds), for longer processing
 	
 	// you only need to pass the name of file as argument for translation (file extension not needed)
-	return shell_exec("python scripts/translate.py " . $filename);
+	// 7.
+	return shell_exec("python scripts/translate.py " . $newFilename);
 
 	//return shell_exec("python scripts/translate.py " . $_FILES["user_file"]['full_path'] . " " . $_FILES['user_file']['name']);
 }
 
 // Language Translation, please check https://rapidapi.com/dickyagustin/api/text-translator2 for more information.
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+	// start of the audio to text translation
+	//	1. Error handling, if user did not select language
+	//	2. Check file format
+	//  3. append user_id into the filename
+	//  4. double check if (audio_files folder exists) and (filename already exists)
+	//	5. Upload the file onto audio_files folder of server (must be updated with user id)
+	//	6. remove background noise/music, get the vocals
+	// 	7. translate the transcribed file
+	
 	$curl = curl_init();
 
-	// Error Handling if user did not select language
+	// 1.
 	if ($_POST["src"] == "" || $_POST['target'] == "") {
         // error, user did not choose language
         logs("error-at", $_SESSION['username'], $dbcon);
@@ -111,17 +143,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         
     } 
 
+	// 2.
 	if(ISSET($_POST["text"])){
 		$transcript = $_POST["text"];
 	}
 	else{
-		$path=$_FILES['user_file']['name'];
-		$transcript = uploadAndTranscribe($path);
+		// required for uploading the file
+		$path=$_FILES['user_file']['name']; // file
+		$userid = $_SESSION['user_id']; // user id needed to separate all files between each user by appending userid to filename
+		
+		// 2. 
+		validateFormat();
+
+		// 3. 4. 5. 6. 7.
+		$transcript = uploadAndTranscribe($path, $userid);
 		echo 'TRANSCRIPT: ' . $transcript;
 	} 
 	
-	// check file format
-	validateFormat();
 
 	$src_lang =  $lang_codes[$_POST["src"]] ?? '';
 	$trg_lang = $lang_codes[$_POST["target"]] ?? '';
