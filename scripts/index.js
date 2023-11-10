1c52f546f8d8195b23cc372676efc5b99d6aaaf8
Bluem972 <104214023+Bluem972@users.noreply.github.com>
2023-11-10 19:52:56 +0800

    Removed search bar eventlistener and queryselector

--- scripts/index.js
+++ scripts/index.js
@@ -17,21 +17,6 @@ menuBar.addEventListener('click', () => {
     sideBar.classList.toggle('close');
 });
 
-const searchBtn = document.querySelector('.content nav form .form-input button');
-const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
-const searchForm = document.querySelector('.content nav form');
-
-searchBtn.addEventListener('click', function (e) {
-    if (window.innerWidth < 576) {
-        e.preventDefault;
-        searchForm.classList.toggle('show');
-        if (searchForm.classList.contains('show')) {
-            searchBtnIcon.classList.replace('bx-search', 'bx-x');
-        } else {
-            searchBtnIcon.classList.replace('bx-x', 'bx-search');
-        }
-    }
-});
 
 window.addEventListener('resize', () => {
     if (window.innerWidth < 768) {
