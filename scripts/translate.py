05702743d7e920ced85152fc0f46abd139734fd1
lyrick <66986241+lyrick17@users.noreply.github.com>
2023-11-16 00:37:46 -0800

    copied translate.py ni jc

--- scripts/translate.py
+++ scripts/translate.py
@@ -5,39 +5,29 @@ import subprocess
 import time
 import io
 
-
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-# model sizes: tiny, base, small, medium, large
 model = whisper.load_model("base")
 
 # TAKE NOTE: make sure the filename does not have any space
 filename = sys.argv[1]
-
+removeBGM = sys.argv[2]
+extension = sys.argv[3]
 if __name__ == '__main__':
-    # specified language is a need so the model won't assume it always as english i guess?
-    #   - the language codes are in notes/language_support.txt
-    #   - kelangan natin na i-connect yung language supported ng whisper at ng api natin
-    # also, plan is model size would come from the user 
-    
+
+    #subprocess.call(['python', 'scripts/separate.py'])
+    #time.sleep(1)
 
     # make sure the audio has been processed in spleeter first        
+
+    # Translates uploaded file directly if checkbox is not checked
+    if removeBGM == "off":
+        result = model.transcribe("audio_files/" + filename + "." + extension)
+        print(result["text"])
     
-    result = model.transcribe("audio_files/" + filename + "/vocals.wav")
-    
-    
-    #result = model.transcribe("audio_files/" + filename + "/vocals.wav", language="tl")
-    print(result["text"])
+    # Translates the extracted vocals if checkbox is checked
+    else:
+        result = model.transcribe("audio_files/" + filename + "/vocals.wav")
+        print(result["text"])
 
 
-#subprocess.call(['python', 'scripts/separate.py'])
-#time.sleep(1)
-
-''' ------- PREVIOUS CODE
-import sys
-import whisper
-
-model = whisper.load_model("base")
-result = model.transcribe('audio_files/' + sys.argv[1])
-print(result["text"])
-'''
