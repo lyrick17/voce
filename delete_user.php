96cd923967725c23ab66d279445fd64acbfdb70a
Bluem <104214023+Bluem972@users.noreply.github.com>
2023-11-28 13:58:16 +0800

    recommit, fixed typo in delete_user.php

--- delete_user.php
+++ delete_user.php
@@ -1,22 +1,21 @@
 <?php
  require("mysql/mysqli_connect.php"); 
 
-//MESSY AS HELL
 //Delete records of a user from all tables in the database
     $q = "SET foreign_key_checks = 0;";
-    msqli_query($dbcon, $q);
+    mysqli_query($dbcon, $q);
 
     $q = "DELETE FROM users WHERE user_id = ". $_POST['userId'] . ";";
-    msqli_query($dbcon, $q);
+    mysqli_query($dbcon, $q);
 
     $q = "DELETE FROM audio_files WHERE user_id = ". $_POST['userId'] . ";";
-    msqli_query($dbcon, $q);
+    mysqli_query($dbcon, $q);
 
     $q = "DELETE FROM text_translations WHERE user_id = ". $_POST['userId'] . ";";
-    msqli_query($dbcon, $q);
+    mysqli_query($dbcon, $q);
 
     $q = "DELETE FROM user_activity_log WHERE user_id = ". $_POST['userId']  . ";";
-    msqli_query($dbcon, $q);
+    mysqli_query($dbcon, $q);
 
     $q = "SET foreign_key_checks = 1;";
     mysqli_query($dbcon, $q);
@@ -26,4 +25,3 @@ $q = "SELECT user_id, username, email, registration_date, type FROM users ORDER 
 $users = mysqli_query($dbcon, $q);
 $result = mysqli_fetch_all($users, MYSQLI_ASSOC);
 exit(json_encode($result));
-
