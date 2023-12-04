7cf8a4dcf0b97fa27d07ef75edc7362677e0c26f
lyrick <66986241+lyrick17@users.noreply.github.com>
2023-12-04 09:59:32 +0800

    Delete scripts/remove_silent.py
    no need na, since the solution is removeSilence method in Translator_Functions, using shell_exec, calling ffmpeg

--- scripts/remove_silent.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def remove_silence(input_file, output_file):
-    command = ['ffmpeg', '-i', input_file, '-af', 'silenceremove=1:0:-50dB', output_file]
-    subprocess.run(command, check=True)
-
-# Use the function
-remove_silence('input.mp3', 'output.mp3')
