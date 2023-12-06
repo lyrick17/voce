6c39ce7f7dd828e63efa0e3d1d0e16ce7bf89dca
lyrick <66986241+lyrick17@users.noreply.github.com>
2023-12-06 13:52:08 +0800

    Updated contact.php
    Added the functionality of contact.php.
    Testing is still not done.

--- utilities/contact.php
+++ utilities/contact.php
@@ -1,24 +1,36 @@
 <?php 
 
-$contact_error = '';
+$contact_message = '';
+$message = array("Thank you for your Feedback!", "Error. Please try again. Thank you.");
+
 // user sends out a feedback
 if($_SERVER['REQUEST_METHOD'] == "POST") {
 
-    // 
-    // 1
-
     $error = 0;
-
+    // server-side validation first before sending the email
     $c_name = (!empty($_POST['contact_name'])) ? $_POST['contact_name'] : $error++;
-    $c_email = (!empty($_POST['contact_email'])) ? $_POST['contact_email'] : $error++;
-    $c_subject = (!empty($_POST['contact_email'])) ? $_POST['contact_subject'] : $error++;
-    $c_message = (!empty($_POST['contact_email'])) ? $_POST['contact_message'] : $error++;
-
+    $c_email = (!empty($_POST['contact_email'])) ? $_POST['contact_email'] : $error++; // the sender email
     if (!filter_var($_POST['contact_email'], FILTER_VALIDATE_EMAIL)) { $error++; }
+    $c_subject = (!empty($_POST['contact_subject'])) ? $_POST['contact_subject'] : $error++;
+    $c_message = (!empty($_POST['contact_message'])) ? $_POST['contact_message'] : $error++;
 
     if ($error > 0) {
-        $contact_error = "Please fill out all the fields. Thank you";
+        $contact_message = $message[0];
+    } else {
+        // provide the content to be sent out onto our email
+        $to_email = 'example@example.com'; // our email, the recipient
+        $email_subject = $c_subject;
+        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=utf-8'];
+        $body_content = ["Name: {$c_name}", "Email: {$c_email}", "Message:", $c_message];
+        $body = join(PHP_EOL, $bodyContent);
+
+        if (mail($to_email, $email_subject, $body, $headers) {
+            $contact_message = $message[1];
+        } else {
+            $contact_message = $message[0];
+        }
     }
+
 }
 
 ?>
