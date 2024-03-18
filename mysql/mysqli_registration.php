396ae616db33c2b2847e23f2c123e2add55c15b3
lyrick <lyrickjonson@gmail.com>
2024-03-18 21:31:17 +0800

    added 3 configurations  ini_set
    ini_set("max_execution_time", 600);
    ini_set('upload_max_filesize', '80M');
    ini_set('post_max_size', '80M');

--- mysql/mysqli_registration.php
+++ mysql/mysqli_registration.php
@@ -11,6 +11,12 @@ if (session_status() == PHP_SESSION_NONE) {
 require("mysqli_connect.php");
 require("mysqli_logs.php");
 
+
+// can be modified in php.ini but this is alternative
+ini_set("max_execution_time", 600);     // 10 minute processing time max
+ini_set('upload_max_filesize', '80M');  // 50MB max file size
+ini_set('post_max_size', '80M');        // post_max_size
+
 // file if user/admin is not logged in
 
 
